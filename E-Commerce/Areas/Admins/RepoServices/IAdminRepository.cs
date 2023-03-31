8932b208d35cb8405ccc4efe5542c1e8046d51f2
Noura Mahmoud <nouramahmoud099@gmail.com>
2023-03-31 13:55:31 +0200

    admin controller

--- E-Commerce/Areas/Admins/RepoServices/IAdminRepository.cs
+++ E-Commerce/Areas/Admins/RepoServices/IAdminRepository.cs
@@ -5,9 +5,10 @@ namespace E_Commerce.Areas.Admins.RepoServices
     public interface IAdminRepository
     {
         public List<Admin> GetAll();
-        public Admin GetDetails(int id);
+        public Admin GetDetailsByID(int id);
+        public Admin GetDetailsByName(string name);
         public void Insert(Admin admin);
-        public void UpdateCrs(int id, Admin admin);
-        public void DeleteCrs(int id);
+        public void UpdateAdmin(int id, Admin admin);
+        public void DeleteAdmin(int id);
     }
 }
--- README.md
+++ README.md
@@ -1,6 +1,12 @@
+1. Create the entities/models of each Area
+    
     ```
     add-migration "initial" -context databaseContext 
     ```
     ```
     Update-Database -context databaseContext
     ```
+2. Create your services interfaces and class
+3. Create read/write controllers
+
+
