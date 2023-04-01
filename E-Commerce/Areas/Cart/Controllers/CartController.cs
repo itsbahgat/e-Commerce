9fb3939993b6df0ac7cb70a192dd0ee72af85192
ahmedkamal <AKreview22@gmail.com>
2023-04-01 04:26:52 +0200

    CART

---
+++ E-Commerce/Areas/Cart/Controllers/CartController.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using E_Commerce.Areas.Cart.Models;
+using E_Commerce.Models;
+
+namespace E_Commerce.Areas.Cart.Controllers
+{
+    //Auto Generated - WE NEED TO UPDATE THIS CONTROLLER BUT FIRST I HAVE TO HAVE A FULLY FINISHED CUSTOMER AND PRODUCT
+    [Area("Cart")]
+    public class CartController : Controller
+    {
+        private readonly databaseContext _context;
+
+        public CartController(databaseContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Cart/Cart
+        public async Task<IActionResult> Index()
+        {
+            var databaseContext = _context.Carts.Include(c => c.Customer);
+            return View(await databaseContext.ToListAsync());
+        }
+
+        // GET: Cart/Cart/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Carts == null)
+            {
+                return NotFound();
+            }
+
+            var cart = await _context.Carts
+                .Include(c => c.Customer)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (cart == null)
+            {
+                return NotFound();
+            }
+
+            return View(cart);
+        }
+
+        // GET: Cart/Cart/Create
+        public IActionResult Create()
+        {
+            ViewData["CustomerId"] = new SelectList(_context.Customers, "Id", "Address");
+            return View();
+        }
+
+        // POST: Cart/Cart/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,CustomerId,IsCompleted")] Models.Cart cart)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(cart);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CustomerId"] = new SelectList(_context.Customers, "Id", "Address", cart.CustomerId);
+            return View(cart);
+        }
+
+        // GET: Cart/Cart/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Carts == null)
+            {
+                return NotFound();
+            }
+
+            var cart = await _context.Carts.FindAsync(id);
+            if (cart == null)
+            {
+                return NotFound();
+            }
+            ViewData["CustomerId"] = new SelectList(_context.Customers, "Id", "Address", cart.CustomerId);
+            return View(cart);
+        }
+
+        // POST: Cart/Cart/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,CustomerId,IsCompleted")] Models.Cart cart)
+        {
+            if (id != cart.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(cart);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CartExists(cart.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CustomerId"] = new SelectList(_context.Customers, "Id", "Address", cart.CustomerId);
+            return View(cart);
+        }
+
+        // GET: Cart/Cart/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Carts == null)
+            {
+                return NotFound();
+            }
+
+            var cart = await _context.Carts
+                .Include(c => c.Customer)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (cart == null)
+            {
+                return NotFound();
+            }
+
+            return View(cart);
+        }
+
+        // POST: Cart/Cart/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Carts == null)
+            {
+                return Problem("Entity set 'databaseContext.Carts'  is null.");
+            }
+            var cart = await _context.Carts.FindAsync(id);
+            if (cart != null)
+            {
+                _context.Carts.Remove(cart);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CartExists(int id)
+        {
+          return (_context.Carts?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ E-Commerce/Areas/Cart/Models/Cart.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using E_Commerce.Areas.Customers.Models;
+using E_Commerce.Areas.Products.Models;
+
+namespace E_Commerce.Areas.Cart.Models
+{
+    public class Cart
+    {
+        public int Id { get; set; }
+
+        public int CustomerId { get; set; }
+
+        [ForeignKey("CustomerId")]
+        public virtual Customer Customer { get; set; }
+
+        public virtual ICollection<CartItem> CartItems { get; set; }
+
+        [NotMapped]
+        public decimal TotalPrice => CartItems?.Sum(x => x.Price * x.Quantity) ?? 0;
+
+        public bool IsCompleted { get; set; }
+
+        public void AddItem(Product product, int quantity)
+        {
+            var existingCartItem = CartItems.FirstOrDefault(x => x.Id == product.Id);
+            if (existingCartItem != null)
+            {
+                existingCartItem.Quantity += quantity;
+            }
+            else
+            {
+                CartItems.Add(new CartItem
+                {
+                    Id = product.Id,
+                    Name = product.Name,
+                    Price = product.Price,
+                    Quantity = quantity
+                });
+            }
+        }
+
+        public void RemoveItem(int productId)
+        {
+            var existingCartItem = CartItems.FirstOrDefault(x => x.Id == productId);
+            if (existingCartItem != null)
+            {
+                CartItems.Remove(existingCartItem);
+            }
+        }
+
+        public void Clear()
+        {
+            CartItems.Clear();
+        }
+
+        public void Checkout()
+        {
+            IsCompleted = true;
+            // Additional logic for order completion (e.g. payment processing, shipping, etc.) could be added here
+        }
+    }
+
+    public class CartItem : Product
+    {
+        public int Quantity { get; set; }
+    }
+}
