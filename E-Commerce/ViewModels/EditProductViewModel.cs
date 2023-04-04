6796c56919c1bd8f238a8366a093e759dfaccc51
Youssef king <yussefbahgat@gmail.com>
2023-04-04 03:12:46 +0200

    Add EditProductViewModel

---
+++ E-Commerce/ViewModels/EditProductViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace E_Commerce.ViewModels
+{
+    public class EditProductViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+
+        public IFormFile Image { get; set; }
+        public string? URL { get; set; }
+    }
+}
